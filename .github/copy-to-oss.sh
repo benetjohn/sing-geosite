6602d6f734eb6e2488b6b767715acbd4b11fa218
Gaoyang Zhang <gy@blurgy.xyz>
2024-11-26 21:46:51 +0800

    Fix oss copying script
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- .github/copy-to-oss.sh
+++ .github/copy-to-oss.sh
@@ -10,12 +10,12 @@ export PATH="$ossutil_dir:$PATH"
 popd
 
 ossutil config -e "$OSS_ENDPOINT" -i "$OSS_ACCESS_KEY" -k "$OSS_ACCESS_KEY_SECRET"
-ossutil cp ../geosite-category-ads-all.srs "oss://$OSS_BUCKET_NAME"
-ossutil cp ../geosite-cn.srs "oss://$OSS_BUCKET_NAME"
+ossutil cp rule-set/geosite-category-ads-all.srs "oss://$OSS_BUCKET_NAME"
+ossutil cp rule-set/geosite-cn.srs "oss://$OSS_BUCKET_NAME"
 
 curl https://cdn.jsdelivr.net/gh/SagerNet/sing-geoip@rule-set/geoip-cn.srs -LO
 curl https://cdn.jsdelivr.net/gh/SagerNet/sing-geoip@rule-set/geoip-jp.srs -LO
 curl https://cdn.jsdelivr.net/gh/SagerNet/sing-geoip@rule-set/geoip-us.srs -LO
-ossutil cp ../geosite-cn.srs "oss://$OSS_BUCKET_NAME"
-ossutil cp ../geosite-jp.srs "oss://$OSS_BUCKET_NAME"
-ossutil cp ../geosite-us.srs "oss://$OSS_BUCKET_NAME"
+ossutil cp geoip-cn.srs "oss://$OSS_BUCKET_NAME"
+ossutil cp geoip-jp.srs "oss://$OSS_BUCKET_NAME"
+ossutil cp geoip-us.srs "oss://$OSS_BUCKET_NAME"
