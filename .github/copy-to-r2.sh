4eabcadca4098426b688131b019b3518b64ffd18
Gaoyang Zhang <gy@blurgy.xyz>
2025-02-17 13:40:22 +0800

    Fix R2 upload checksum algorithm again
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- .github/copy-to-r2.sh
+++ .github/copy-to-r2.sh
@@ -9,14 +9,16 @@ aws_access_key_id=${R2_ACCESS_KEY_ID}
 aws_secret_access_key=${R2_ACCESS_KEY_SECRET}
 EOF
 
-# Upload locally built files
-aws s3 cp rule-set/geosite-category-ads-all.srs "s3://${R2_BUCKET_NAME}/" \
-  --endpoint-url "https://${CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com" \
-  --force
+aws_cli_cp_args="--endpoint-url https://${CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com --checksum-algorithm CRC32 --force"
 
+# Upload locally built files
+# shellcheck disable=SC2086
+aws s3 cp rule-set/geosite-category-ads-all.srs "s3://${R2_BUCKET_NAME}/" \
+  $aws_cli_cp_args
+
+# shellcheck disable=SC2086
 aws s3 cp rule-set/geosite-cn.srs "s3://${R2_BUCKET_NAME}/" \
-  --endpoint-url "https://${CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com" \
-  --force
+  $aws_cli_cp_args
 
 # Download and upload external files
 curl --fail https://raw.githubusercontent.com/SagerNet/sing-geoip/rule-set/geoip-cn.srs -LO
@@ -25,8 +27,7 @@ curl --fail https://raw.githubusercontent.com/SagerNet/sing-geoip/rule-set/geoip
 
 # Upload downloaded files to R2
 for file in geoip-{cn,jp,us}.srs; do
+  # shellcheck disable=SC2086
   aws s3 cp "$file" "s3://${R2_BUCKET_NAME}/" \
-    --endpoint-url "https://${CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com" \
-    --checksum-algorithm CRC32 \
-    --force
+    $aws_cli_cp_args
 done
